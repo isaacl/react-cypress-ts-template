fe64df52ab1e1e2dc0fba2e83fa8a2d87b6baa51
Isaac Levy <isaac.r.levy@gmail.com>
2024-07-16 12:15:16 -0400

    ch03.2

--- src/components/HeaderBarBrand.cy.tsx
+++ src/components/HeaderBarBrand.cy.tsx
@@ -1,9 +1,13 @@
-// src/components/HeaderBarBrand.cy.tsx
+// src/components/HeaderBarBrand.tsx
 import HeaderBarBrand from './HeaderBarBrand'
-import '../styles.scss' // technically not needed
+import '../styles.scss'
 
 describe('HeaderBarBrand', () => {
   it('should', () => {
     cy.mount(<HeaderBarBrand />)
+    cy.get('a')
+      .should('have.attr', 'href', 'https://reactjs.org/')
+      .and('have.attr', 'target', '_blank')
+      .and('have.attr', 'rel', 'noopener noreferrer')
   })
 })
--- src/components/HeaderBarBrand.tsx
+++ src/components/HeaderBarBrand.tsx
@@ -1,5 +1,12 @@
 // src/components/HeaderBarBrand.tsx
+import {FaReact} from 'react-icons/fa'
 
 export default function HeaderBarBrand() {
-  return <div>hello</div>
+  return (
+    <div>
+      <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+        <FaReact />
+      </a>
+    </div>
+  )
 }
