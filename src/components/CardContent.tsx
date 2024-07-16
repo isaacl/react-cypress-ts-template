5517b89dda7249f0a5c451a8a71c0a4b6ae33729
Isaac Levy <isaac.r.levy@gmail.com>
2024-07-16 11:23:41 -0400

    ch02.1

---
+++ src/components/CardContent.tsx
@@ -0,0 +1,15 @@
+import '../styles.scss'
+
+type CardContentProps = {
+  name: string
+  description: string
+}
+
+export default function CardContent({name, description}: CardContentProps) {
+  return (
+    <div data-cy="card-content" className="card-content">
+      <div className="name">{name}</div>
+      <div className="description">{description}</div>
+    </div>
+  )
+}
